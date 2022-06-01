d146d57a45a31bf9e4fce87b4e5bf78b37fab06d
woranoIX <75521927+woranonIX@users.noreply.github.com>
2022-06-01 20:54:04 +0700

    Setup
    set up all project needed.
    I hope your enjoy my environment.

---
+++ src/pages/home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref, onMounted, computed, reactive } from "vue";
+</script>
+
+<template>
+    <h1>Home for router</h1>
+</template>
+
+<style scoped>
+
+</style>
